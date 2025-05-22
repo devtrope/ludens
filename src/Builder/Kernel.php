9eda1cb4be24141ce9c2a2b42f1ad7d2cb35e364
Quentin SCHIFFERLE <dev.trope@gmail.com>
2025-05-22 16:35:51 +0200

    Removed unwanted character

--- src/Builder/Kernel.php
+++ src/Builder/Kernel.php
@@ -4,8 +4,14 @@ namespace Ludens\Builder;
 
 class Kernel
 {
+    /**
+     * Handles the incoming request and resolves the route.
+     *
+     * @param \Ludens\Http\Request $request
+     * @return void
+     */
     public static function handle(\Ludens\Http\Request $request): void
-    {;
+    {
         try {
             \Ludens\Http\Router::resolve(
                 new \Ludens\Http\Route($request),
