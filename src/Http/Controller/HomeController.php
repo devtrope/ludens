8fbcd994ff6d080392701b70d562022e0ee2c75c
Quentin SCHIFFERLE <dev.trope@gmail.com>
2026-03-17 13:51:49 +0100

    remove unused classes

--- src/Http/Controller/HomeController.php
+++ src/Http/Controller/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Http\Controller;
 
-use App\Http\Repository\RoleRepository;
-use App\Http\Repository\UserRepository;
-
 class HomeController extends BaseController
 {
     public function index(): void
