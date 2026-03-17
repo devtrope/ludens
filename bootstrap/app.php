8689aed383471be9f68a248d8f409c6dec2876f5
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2026-03-17 10:12:43 +0100

    fix coding standards

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,6 @@ use Ludens\Core\Application;
 
 $app = Application::getInstance();
 $app->usePathsFrom(require __DIR__ . '/../config/paths.php');
-$app->loadEnvironmentFrom(__DIR__ . '/../.env'); 
+$app->loadEnvironmentFrom(__DIR__ . '/../.env');
 $app->loadConfiguration();
 return $app;
--- src/Http/Controller/HomeController.php
+++ src/Http/Controller/HomeController.php
@@ -1,5 +1,9 @@
 <?php
+
 namespace App\Http\Controller;
+
+use App\Http\Repository\RoleRepository;
+use App\Http\Repository\UserRepository;
 
 class HomeController extends BaseController
 {
