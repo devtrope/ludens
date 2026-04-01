f4074c7301785e9f3b127a8b07953630ed60f4b4
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2026-04-01 11:39:10 +0200

    remake of the welcome page

--- src/Http/Controller/HomeController.php
+++ src/Http/Controller/HomeController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controller;
 
 use Ludens\Http\Response;
+use Composer\InstalledVersions;
 
 class HomeController extends BaseController
 {
     public function index(): Response
     {
-        return $this->view('home/index.html.twig');
+        return $this->view('home/index.html.twig', [
+            'php_version' => PHP_VERSION,
+            'ludens_version' => InstalledVersions::getVersion('devtrope/framework')
+        ]);
     }
 }
