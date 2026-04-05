9a5742cbed078df82d80bbba849cc15050274b69
Quentin SCHIFFERLE <q.schifferle@tod.fr>
2026-04-05 10:56:35 +0200

    release v0.1.6

--- src/Model/User.php
+++ src/Model/User.php
@@ -2,17 +2,34 @@
 
 namespace App\Model;
 
-class User
+use Ludens\Framework\ModelInterface;
+
+class User implements ModelInterface
 {
     private int $id;
 
-    public function id()
+    public function id(): int
     {
         return $this->id;
     }
 
-    public function setId(int $id)
+    public function setId(int $id): void
     {
         $this->id = $id;
+    }
+
+    public function hasOne(): array
+    {
+        return [];
+    }
+
+    public function rules(): array
+    {
+        return [];
+    }
+
+    public function fillable(): array
+    {
+        return [];
     }
 }
